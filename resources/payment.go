66e333814456b74d99941d0209ce96edb69b0065
Nikita Shaburov <nikita.shaburov1@gmail.com>
2021-12-28 15:00:47 +0200

    edit payment struct

--- resources/payment.go
+++ resources/payment.go
@@ -3,12 +3,12 @@ package resources
 import "time"
 
 type Payment struct {
-	PaymentID    string                 `json:"payment_id"`
-	ContractHash string                 `json:"contract_hash"`
-	PaymentState string                 `json:"payment_state"`
-	ModifiedAt   time.Time              `json:"modified_at"`
-	Contract     PaymentContract        `json:"contract"`
-	UserInfo     map[string]interface{} `json:"user_info"`
+	PaymentID    string                   `json:"payment_id"`
+	ContractHash string                   `json:"contract_hash"`
+	PaymentState string                   `json:"payment_state"`
+	ModifiedAt   time.Time                `json:"modified_at"`
+	Contract     PaymentContract          `json:"contract"`
+	UserInfo     []map[string]interface{} `json:"user_info"`
 }
 
 type PaymentContract struct {
@@ -19,4 +19,4 @@ type SettlePayment struct{}
 
 type LockPayment struct{}
 
-type CompletePayment struct {}
+type CompletePayment struct{}
